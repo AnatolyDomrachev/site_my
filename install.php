a66f954ee5bbf649ba48cf608fac6e5b2d6575db
Ваше Имя <you@example.com>
2020-08-23 14:07:09 +0300

    1

--- install.php
+++ install.php
@@ -43,5 +43,22 @@ foreach ($tables as $table => $cols)
 	    echo "Удалось создать таблицу\n" ;
 
 	echo"\n\n\n";
-}	 
+}
+
+$querys[] = "insert into groups (name) values ('admin')";
+$querys[] = "insert into users (name,group_id,passwd) select 'admin' , id , '1111' from groups where name = 'admin'";
+
+foreach ($querys as $query)
+{
+	print_r($query);
+
+	if ( !$mysqli->query($query) ) {
+	    echo "Не удалось создать таблицу: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	else
+	    echo "Удалось создать таблицу\n" ;
+
+	echo"\n\n\n";
+}
+	 
 ?>
