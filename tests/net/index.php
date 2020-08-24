fbaad39199ea4bfad6ac054eb54d800bd3171384
Ваше Имя <you@example.com>
2020-08-24 12:57:45 +0300

    1

---
+++ tests/net/index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>123</title>
+ </head>
+ <body>
+
+<p>
+<a href = test_123> Тест, вопросы подряд </a>
+<a href = test_321> Тест, вопросы не подряд </a>
